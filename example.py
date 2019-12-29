25b2a6d01fded0cd9e232c07104337c80373da4c
Kaitai Dong <44104289+KaitaiD@users.noreply.github.com>
2019-12-29 18:02:27 +0000

    update example.py

--- example.py
+++ example.py
@@ -1,3 +1,17 @@
+import os
+import time
+import logging
+
+import stomp
+import sqlite3
+import pandas as pd
+
+from listener import MVListener, PPMListener, VSTPListener, TDListener, MessagerToSQL
+from topicmapping import topic_mapping
+
+pd.options.display.max_columns = None
+
+# four topics to choose from - 1. MVT 2. PPM 3. VSTP 4. TD
 TOPIC = "MVT"
 
 train_rdf = RailDataFeeder(
@@ -12,3 +26,4 @@ train_rdf = RailDataFeeder(
 )
 
 train_rdf.download_feed()
+train_ppm_rdf.to_pandas()
